941d1c3e98576a114fb4dbd5c27e16988e5ee2f8
lina_aziz <lenaexo0@gmail.com>
2026-01-04 13:24:58 +0300

    Update __init__.py

--- part2/hbnb/app/models/__init__.py
+++ part2/hbnb/app/models/__init__.py
@@ -2,5 +2,7 @@ from .base_model import BaseModel
 from .user import User
 from .amenity import Amenity
 from .place import Place
+from .review import Review
 
-__all__ = ["BaseModel", "User", "Amenity", "Place"]
+__all__ = ["BaseModel", "User", "Amenity", "Place", "Review"]
+
