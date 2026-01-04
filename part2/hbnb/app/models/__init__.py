0fd1dfb79bbb9c3b7d2271425bfd5284f6673c06
lina_aziz <lenaexo0@gmail.com>
2026-01-04 13:08:26 +0300

    Add Place to models' __all__ export

--- part2/hbnb/app/models/__init__.py
+++ part2/hbnb/app/models/__init__.py
@@ -1,8 +1,9 @@
 from .base_model import BaseModel
 from .user import User
 from .amenity import Amenity
+from .place import Place
+__all__ = ["BaseModel", "User", "Amenity", "Place"]
 
-__all__ = ["BaseModel", "User", "Amenity"]
 
 
 
