906b0c91e05bf08b79e9d14b612c3a9d83ece2c5
lina_aziz <lenaexo0@gmail.com>
2026-01-11 00:24:54 +0300

    Update repository.py

--- part2/hbnb/app/persistence/repository.py
+++ part2/hbnb/app/persistence/repository.py
@@ -1,29 +1,21 @@
-from abc import ABC, abstractmethod
+#!/usr/bin/python3
+"""In-memory repository implementation"""
 
-class Repository(ABC):
-    @abstractmethod
+class Repository:
     def add(self, obj):
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def get(self, obj_id):
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def get_all(self):
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def update(self, obj_id, data):
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def delete(self, obj_id):
-        pass
-
-    @abstractmethod
-    def get_by_attribute(self, attr_name, attr_value):
-        pass
+        raise NotImplementedError
 
 
 class InMemoryRepository(Repository):
@@ -31,22 +23,38 @@ class InMemoryRepository(Repository):
         self._storage = {}
 
     def add(self, obj):
-        self._storage[obj.id] = obj
+        """Add object to storage"""
+        self._storage[str(obj.id)] = obj
+        return obj
 
     def get(self, obj_id):
-        return self._storage.get(obj_id)
+        """Get object by id"""
+        return self._storage.get(str(obj_id))
 
     def get_all(self):
+        """Return all objects"""
         return list(self._storage.values())
 
     def update(self, obj_id, data):
+        """Update object by id"""
         obj = self.get(obj_id)
-        if obj:
-            obj.update(data)
+        if not obj:
+            return None
+
+        for key, value in data.items():
+            if hasattr(obj, key) and value is not None:
+                setattr(obj, key, value)
+
+        return obj
 
     def delete(self, obj_id):
-        if obj_id in self._storage:
-            del self._storage[obj_id]
+        """Delete object by id"""
+        return self._storage.pop(str(obj_id), None)
 
     def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
+        """Get object by attribute (e.g. email)"""
+        return next(
+            (obj for obj in self._storage.values()
+             if getattr(obj, attr_name, None) == attr_value),
+            None
+        )
